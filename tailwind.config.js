2ce482e932fdb3a783e0283a7a8ebdb19e1c8f8a
13right <lysterliwanag011@outlook.com>
2024-04-30 22:23:46 +0800

    main

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors:{
+      Likha:'#f7f8ea',
+      hehe:'#92fec2',
+      sign: '#f8ecdc',
+      outline: '#f17d55'
+    },
+    fontFamily:{
+      libre: ['Libre Baskerville']
+    }
+  },
+  plugins: [],
+}
