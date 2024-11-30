430689add460bbbee1ab8d388732106d63bc649d
13right <lysterliwanag011@outlook.com>
2024-12-01 03:24:51 +0800

    qwerty

--- public/Nav.js
+++ public/Nav.js
@@ -851,6 +851,12 @@ function scrollToBottom() {
     }
 }
 
+function Menu(e){
+    let list = document.querySelector('ul');
+    if(window.innerWidth < 768){
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]'),list.classList.add('opacity-100')) : (e.name = "menu",list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+    }
+}
 
 document.getElementById('message-form').addEventListener('submit', function (e) {
     e.preventDefault();
